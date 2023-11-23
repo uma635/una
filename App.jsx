c2b902098841c3f2ec08a89d2eae0652c578397a
uma635 <146613184+uma635@users.noreply.github.com>
2023-11-23 20:04:17 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,57 @@
+
+// import './App.css'
+// import Counter from './Counter'
+// import LikeButton from './LikeButton'
+// function App() {
+
+ 
+
+//   return (
+//     <>
+//    <p>state in react</p>
+//    <LikeButton/>
+//     </>
+//   )
+// }
+
+// export default App
+
+//lurdoboard
+
+// import LudoBoard from "./LudoBoard"
+// import './App.css'
+// import TodoList from "./TodoList"
+// function App(){
+
+// return(
+//   <>
+//     <TodoList/>
+//   </>
+// )
+
+
+
+// }
+
+// export default App
+
+
+//lottery game 
+
+
+import './App.css';
+
+import Lottery from "./Lottery";
+function App(){
+
+return(
+  <>
+    <Lottery/>
+  </>
+)
+
+
+
+}
+
+export default App
